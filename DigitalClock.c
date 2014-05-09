51475ebb5ee5f0e6a8c80cdd788e83cff3f1dcd1
Aman Agarwal <agarwal_a@live.com>
2014-05-09 06:48:50 +0530

    Update DigitalClock.c
    Added an alarm to play a sound at a given time.

--- DigitalClock.c
+++ DigitalClock.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
 #include <time.h>
 
+int hr, mn, b=0;
+
 void showtime()
 {
-    time_t rawtim;
-    struct tm*  time_;
 
-    time(&rawtim);
-    time_ = localtime(&rawtim);
+    time_t rawtime;
+    struct tm* time_;
 
-    printf("%i/%i/%i %i:%i:%i\n", time_->tm_mday, time_->tm_mon+1, time_->tm_year+1900, time_->tm_hour, time_->tm_min, time_->tm_sec);
+    time(&rawtime);
+    time_=localtime(&rawtime);
+	if(b==1)
+	{
+		if(hr==time_->tm_hour && mn==time_->tm_min)
+		{
+			printf("ALARM");
+			system("open -a VOX Alarm_Tune.mp3"); //This line works for MAC OS only. Check the line how to run a mp3 file in your OS and replace it
+			exit(0);
+		}
+	}
+	printf("%d/%d/%d %d:%d:%d\n", time_->tm_mday, time_->tm_mon+1, time_->tm_year+1900, time_->tm_hour, time_->tm_min, time_->tm_sec);
 }
 
 
 int main(int argc, char *argv[])
 {
 	int a;
+	printf("Do you want to add an alarm? ");
+	scanf("%d", &b);
+	if(b==1)
+	{
+		printf("Enter the Hour and Minutes\n");
+		scanf("%d:%d", &hr, &mn);
+	}
+
 	a=0;
 	while(a!=1)
 	{
